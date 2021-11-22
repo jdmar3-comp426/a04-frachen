39f26bdac218b0e4194c510ac5540af6e3cdb143
frachen <59738945+frachen@users.noreply.github.com>
2021-11-22 13:17:33 -0500

    test

--- server.js
+++ server.js
@@ -40,11 +40,9 @@ app.get("/app/users", (req, res) => {
 
 // READ a single user (HTTP method GET) at endpoint /app/user/:id
 app.get("/app/users/:id", (req, res) => {
-	const stmt = db.prepare("SELECT * FROM userinfo WHERE id = ?");
-	const output = stmt.get("id");
-	console.log(output)
+	const stmt = db.prepare("SELECT * FROM userinfo WHERE id = ?").get(req.params.id);
 	res.status(200).json(stmt);
-})
+});
 
 // UPDATE a single user (HTTP method PATCH) at endpoint /app/update/user/:id
 
